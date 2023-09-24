bf0fcae351e85ddffab53938d5d3fc32330c96af
laizy <zlai7@illinois.edu>
2023-09-24 17:35:50 -0500

    update

--- IR.sv
+++ IR.sv
@@ -1,6 +1,14 @@
 module IR(
     input logic Clk,
+    input logic [15:0] BUS,
+    input logic LD_IR,
+    output logic [15:0] IR
 
 
-);
+);  
+        always_ff @(posedge Clk) begin
+            if(LD_IR) begin
+                IR <= BUS;
+            end
+        end
 endmodule
